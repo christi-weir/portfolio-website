10ebfc2f416feb989eba83c2985634350468df45
christi-weir <christistringer@gmail.com>
2021-05-01 15:38:45 -0400

    added hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Well hello there!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
